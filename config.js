27dcec0c99769531243dba7acac6ac5ab357178a
beron <bleink@outlook.es>
2021-06-02 11:08:45 +0200

    add words to calendars in biography & prod mode

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const softwareState = 'dev'
+const softwareState = 'prod'
 const getUrls = (mode = softwareState) => {
     switch (mode) {
         case 'dev': return {
@@ -6,6 +6,13 @@ const getUrls = (mode = softwareState) => {
                 base: 'http://localhost:7000',
                 getStatus: 'http://localhost:7000/appointment/status',
                 put: 'http://localhost:7000/appointment'
+            }
+        }
+        case 'prod': return {
+            appointment: {
+                base: 'https://codeship-fs.herokuapp.com',
+                getStatus: 'https://codeship-fs.herokuapp.com/appointment/status',
+                put: 'https://codeship-fs.herokuapp.com/appointment'
             }
         }
     }
