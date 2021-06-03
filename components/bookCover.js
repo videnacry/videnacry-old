feef36864425123e9e70cce944d1a26f45dba728
beron <bleink@outlook.es>
2021-06-03 19:55:56 +0200

    add styles to scroll for text in bookCover, change html, css to center bookCovers when image without height/width to fill screen, coverBook parts now on click be in very center

--- components/bookCover.js
+++ components/bookCover.js
@@ -1,18 +1,20 @@
 const bookCover = {}
 bookCover.html = `
 <article class="book-cover-c">
+	<div>
 	<section class="star-out">
 		<img src="./public/img/magic/Role.jpg" />
 		<p>En nuestra realidad siempre encontraremos que existen caminos opuestos en cuanto a su objetivo, de este modo se rige lo que conocemos y lo usamos para llegar a obtener algo (una bebida dulce por ejemplo), pero estos caminos que cruzamos nos llevan a buscar otro (una bebida amarga) o seguir por uno de sus similares (una bebida muy dulce) y los caminos nos van formando, para comprender la trascendencia de esto vasta con saber que "elegir" es un camino y con esto en cuenta,  descubrimos en este cuadro que habla sobre el camino utilizando por medida al ser humano, que cada color representa un camino y el cambio de ser una cosa a otra viene dado por la forma (el hombre azul (soso en caso que subsita) en león (animal que vive por la naturaleza); el feto rojo (ser del que poco depende su vida) en bebe (qué ya puede llorar enérgicamente) y el mismo conjunto es algo que comenzó siendo un tren sobre las ruedas de un tren invertido (transportes qué crecen por lo que llevan y son impulsados el uno por el otro (como las armas y los escudos)), para terminar diré que el verde representa a la naturaleza, el rojo al autocomplacimiento, el azul al rechazo qué es movido por el instinto de supervivencia, el negro a la libre elección (sobreponiendose incluso a lo natural, por ello lleva una forma tan rara y hace de arma y cura,  utiliza las nociones abstractas de lo que percibe), el café al conflicto (a favor y en contra, permanece en  estado contemplativo a través de sus gafas), y el blanco es el infinito qué se puede encontrar en todas partes y contener en palabras... Eres libre de analizar el cuadro con y sin las luces que te he dado, puedes tambi&eacute;n contactarme.</p>
 	</section>
 	<img src="./public/img/magic/Role.jpg" class="star-out"/>
+	<div>
 </article>
 `
 bookCover.init = ({imgSrc, stellarRatio = 2, story, selector, alt = 'picture made with ink', flip = false}) => {
 	$(selector).html(bookCover.html)
 	if (flip) $(selector + ' article').addClass('flipped')
-	const sectionElmt = $(selector + ' article>section')
-	const imgElmt = $(selector + ' article>img')
+	const sectionElmt = $(selector + ' article>div>section')
+	const imgElmt = $(selector + ' article>div>img')
 
 	$(selector + ' img').prop({src: imgSrc, alt})
 	imgElmt.click(function() {
